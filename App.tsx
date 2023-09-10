5efa7d9a644fbc9fa890c2c5f1da4b844ab3a4c1
Lucas Matheus <lucasmgsilva@hotmail.com>
2023-09-10 20:53:07 -0300

    Utilizando tema

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "styled-components/native";
 import { Groups } from "./src/screens/Groups";
-import theme from "./src/theme";
+import theme from "src/theme";
 
 export default function App() {
   return (
