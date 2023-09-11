7a8bcf9bde38df124ea88aa1e68f083f8f09a6d0
Lucas Matheus <lucasmgsilva@hotmail.com>
2023-09-11 13:55:02 -0300

    Componente List Empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,7 @@ import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { useState } from "react";
 import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Groups() {
     const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
@@ -19,6 +20,10 @@ export function Groups() {
                 keyExtractor={group => group}
                 renderItem={({ item: group }) => (
                     <GroupCard title={group} />
+                )}
+                contentContainerStyle={groups.length === 0 && { flex: 1 }}
+                ListEmptyComponent={() => (
+                    <ListEmpty message="Que tal cadastrar a primeira turma?" />
                 )}
             />
 
