94399d484507d3aa842c57c7d4110a2f11988343
Lucas Matheus <lucasmgsilva@hotmail.com>
2023-09-10 23:36:06 -0300

    Fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { ThemeProvider } from "styled-components/native";
 import { Groups } from "./src/screens/Groups";
-import theme from "src/theme";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto"; 
+import theme from "./src/theme";
+import { ActivityIndicator } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {
+        fontsLoaded ? <Groups /> : <ActivityIndicator />
+      }
     </ThemeProvider>
   );
 }
