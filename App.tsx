57633cee2ca4c334acbc49f0487e24fc8e7e90e2
Lucas Matheus <lucasmgsilva@hotmail.com>
2023-09-12 18:49:23 -0300

    Recap

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/
 import theme from "./src/theme";
 import { Loading } from "@components/Loading";
 import { StatusBar } from "react-native";
-import { Players } from "@components/Players";
+import { Groups } from "@screens/Groups";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -15,7 +15,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Players /> : <Loading /> }
+      { fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
   );
 }
