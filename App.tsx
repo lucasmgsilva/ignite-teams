d5036edbe9b93e04bd80be5677d2b515ffd389b9
Lucas Matheus <lucasmgsilva@hotmail.com>
2023-09-12 13:01:39 -0300

    Criando Interface NewGroup

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import { ThemeProvider } from "styled-components/native";
-import { Groups } from "./src/screens/Groups";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto"; 
 import theme from "./src/theme";
 import { Loading } from "@components/Loading";
 import { StatusBar } from "react-native";
+import { NewGroup } from "@screens/NewGroup";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -15,7 +15,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Groups /> : <Loading /> }
+      { fontsLoaded ? <NewGroup /> : <Loading /> }
     </ThemeProvider>
   );
 }
